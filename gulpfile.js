b5e04cc897ea73c7cfeed25397b6df5c32a9a319
Karthik Nayak <karunayak63@gmail.com>
2023-01-18 07:05:32 +0530

    add gulpfile
    * Task to minify css and js.
    * Task to compile sass.

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,55 @@
-const { src, dest, series, watch } = require('gulp');
+const { src, dest, parallel, series, watch } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const minifyJs = require('gulp-uglify');
+const minifyCss = require('gulp-uglifycss');
 
 function buildCSS() {
   return src(['./sass/**/*.scss'])
+    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(
+      minifyCss({
+        uglyComments: true,
+      })
+    )
+    .pipe(dest('dist'));
+}
+
+function buildDocs() {
+  return src(['./documentation/docs.scss'])
     .pipe(sass().on('error', sass.logError))
     .pipe(dest('dist'));
 }
 
+function minifyJS() {
+  return src('./documentation/prism/prism.js')
+    .pipe(minifyJs())
+    .pipe(dest('./documentation/prism'));
+}
+
+function minifyCSS() {
+  return src('./documentation/prism/*.css')
+    .pipe(
+      minifyCss({
+        uglyComments: true,
+      })
+    )
+    .pipe(dest('./documentation/prism'));
+}
+
 function watchTask() {
   watch(['./sass/**/*.scss'], buildCSS);
+  watch(['./documentation/docs.scss'], buildDocs);
+  watch(['./documentation/prism/*.js'], minifyJS);
+  watch(['./documentation/prism/*.css'], minifyCSS);
 }
 
 exports.build = buildCSS;
+exports.buildDocs = buildDocs;
+exports.minifyCSS = minifyCSS;
+exports.minifyJS = minifyJS;
 exports.watch = watchTask;
-exports.default = series(buildCSS, watchTask);
+
+exports.default = series(
+  parallel(buildCSS, buildDocs, minifyCSS, minifyJS),
+  watchTask
+);
